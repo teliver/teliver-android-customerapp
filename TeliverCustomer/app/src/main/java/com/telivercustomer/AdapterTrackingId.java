380b80a1ec9f38fa832a09ecbcca3dfd1543958d
SelvakumarP_Quadkast <selvakumar.p@quadkast.co.in>
2017-07-20 20:59:57 +0530

    Customer Application to track a driver using Teliver sdk

---
+++ TeliverCustomer/app/src/main/java/com/telivercustomer/AdapterTrackingId.java
@@ -0,0 +1,96 @@
+package com.telivercustomer;
+
+import android.app.Activity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.teliver.sdk.core.Teliver;
+import com.teliver.sdk.core.TrackingListener;
+import com.teliver.sdk.models.MarkerOption;
+import com.teliver.sdk.models.TLocation;
+import com.teliver.sdk.models.TrackingBuilder;
+
+import java.util.ArrayList;
+
+
+public class AdapterTrackingId extends RecyclerView.Adapter<AdapterTrackingId.MyViewHolder> {
+
+    private Activity context;
+
+    private ArrayList<Model> listTrackingId;
+
+    public AdapterTrackingId(Activity context) {
+        this.context =context;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.adpater_tracking_id,parent,false);
+        return new MyViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Model model = listTrackingId.get(position);
+        holder.txtTrackingId.setText(model.getTrackingId());
+
+        holder.crdTrackingId.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                TrackingBuilder builder = new TrackingBuilder(new MarkerOption(holder.txtTrackingId.getText().toString().trim()))
+                        .withListener(new TrackingListener() {
+                            @Override
+                            public void onTrackingStarted(String trackingId) {
+                                Log.d("TELIVER::", "onTrackingStarted: " + trackingId);
+                            }
+
+                            @Override
+                            public void onLocationUpdate(String trackingId, TLocation location) {
+                                Log.d("TELIVER::", "onLocationUpdate: " + location.getLatitude() + location.getLongitude());
+                            }
+
+                            @Override
+                            public void onTrackingEnded(String trackingId) {
+                                Log.d("TELIVER::", "onTrackingEnded: " + trackingId);
+                            }
+
+                            @Override
+                            public void onTrackingError(String reason) {
+                                Log.d("TELIVER::", "onTrackingError: " + reason);
+                            }
+                        });
+                Teliver.startTracking(builder.build());
+            }
+
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        Log.d("TELIVER::", "size of list in adapter" + listTrackingId.size());
+        return listTrackingId.size();
+    }
+
+    public void setData(ArrayList<Model> listTrackingId) {
+        this.listTrackingId = listTrackingId;
+    }
+
+
+    class MyViewHolder extends RecyclerView.ViewHolder{
+
+        private TextView txtTrackingId;
+
+        private CardView crdTrackingId;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            txtTrackingId = (TextView) itemView.findViewById(R.id.txtTrackingId);
+            crdTrackingId = (CardView) itemView.findViewById(R.id.crdTrackingId);
+        }
+    }
+}
---
+++ TeliverCustomer/app/src/main/java/com/telivercustomer/Database.java
@@ -0,0 +1,32 @@
+package com.telivercustomer;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class Database extends SQLiteOpenHelper {
+
+    private final static int DATABASEVERSION = 1;
+
+    private final static String DATABASENAME = "teliver.db";
+
+    public String TABLENAME = "tracking_details",MESSAGE= "message",TRACKING_ID = "tracking_id";
+
+    public Database(Context context) {
+        super(context, DATABASENAME, null, DATABASEVERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE " + TABLENAME + "( " + MESSAGE + " TEXT,"  + TRACKING_ID + " TEXT PRIMARY KEY" + ")";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXITS" + TABLENAME);
+        onCreate(db);
+    }
+}
+
